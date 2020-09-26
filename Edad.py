e0c58d4210e1dd39165a3ce63f0d501a26f34f78
Angie Otaya <angieotaya4@gmail.com>
2020-09-25 23:21:36 -0500

    calcula tu edad apartir del año actual

---
+++ Edad.py
@@ -0,0 +1,11 @@
+Fa = int
+Fa = 2020
+nombre = input("Digite su Nombre: ")
+Fn = int(input("Digite su Año de Nacimiento: "))
+
+EdadActual = 0
+
+EdadActual = (Fa - Fn)
+
+print ("SU NOMBRE ES:",nombre)
+print ("SU EDAD ACTUAL ES:",EdadActual)
