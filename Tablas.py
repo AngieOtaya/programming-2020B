110fa58020626b06cc72a231bb139f23c1d891c1
Angie Otaya <angieotaya4@gmail.com>
2020-09-26 00:00:09 -0500

    Tablas de Multiplicar

---
+++ Tablas.py
@@ -0,0 +1,35 @@
+n1 = int
+n2 = int
+n3 = int
+n4 = int
+n5 = int
+n6 = int
+n7 = int
+n8 = int
+n9 = int
+
+
+n = int(input("Digite un Numero: "))
+
+
+
+n1 = n*1
+n2 = n*2
+n3 = n*3
+n4 = n*4
+n5 = n*5
+n6 = n*6
+n7 = n*7
+n8 = n*8
+n9 = n*9
+n10 = n*10
+print ("El calculo Uno    es: ", n1)
+print ("El calculo dos    es: ", n2)
+print ("El calculo tres   es: ", n3)
+print ("El calculo cuatro es: ", n4)
+print ("El calculo cinco  es: ", n5)
+print ("El calculo seis   es: ", n6)
+print ("El calculo siete  es: ", n7)
+print ("El calculo ocho   es: ", n8)
+print ("El calculo nueve  es: ", n9)
+print ("El calculo diez   es: ", n10)
